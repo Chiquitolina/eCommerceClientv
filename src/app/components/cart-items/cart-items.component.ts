e64627f92f7e6c1173718a061ee62fac09b3fc9f
Alejandro <aleig.906@gmail.com>
2024-10-05 15:03:19 -0300

    feat: cart items component

--- src/app/components/cart-items/cart-items.component.ts
+++ src/app/components/cart-items/cart-items.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../services/cart/cart.service';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-cart-items',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './cart-items.component.html',
   styleUrl: './cart-items.component.scss'
 })
 export class CartItemsComponent {
+
+  cartServ = inject(CartService)
 
 }
